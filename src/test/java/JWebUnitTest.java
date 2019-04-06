941e0cafe4636d4c8b5886f5650481ba0bac6674
CoffeeComputer <jacobcduane@gmail.com>
2019-04-06 17:55:15 +0100

    Tests working with Selenium

--- src/test/java/JWebUnitTest.java
+++ src/test/java/JWebUnitTest.java
@@ -1,55 +1,49 @@
 package uk.uoa.cs.princSwEng.test;
 
-
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
-import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
-import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class JWebUnitTest {
 
-    
-    @Before
-    public void prepare() {
-        setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
-        setBaseUrl("http://localhost:8080/Echo-Team/");
-    }
-
     @Test
-    public void foo() {
+    public void fooTest() {
 
     }
-    
+    @Test
+    public void firstTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8080/Echo-Team-0.1/");
+        assertEquals("MetaTranslate", driver.getTitle(), "Title check failed!");
+        driver.close();
+        driver.quit();
+    }
+
+
+    /*
     @Test
     public void testIndexPage() {
-        beginAt("http://localhost:8080/Echo-Team-0.1/"); 
-        assertTitleEquals("MetaTranslate");
-        
-        /*
+        beginAt("http://localhost:8080/Echo-Team/");
+        assertTitleEquals("Metatranslate");
         assertLinkPresent("manager");
         clickLink("manager");
         assertTitleEquals("Create Survey");
         System.out.println("testIndexPage done!");
-        */
-        
     }
-    /*
-    
+
     @Test
     public void testManagerPage() {
-        beginAt("http://localhost:8080/Echo-Team/manager"); 
+        beginAt("http://localhost:8080/Echo-Team/manager");
         assertTitleEquals("Create Survey");
         assertLinkPresent("index");
         clickLink("index");
         assertTitleEquals("Metatranslate");
     }
-    */
+
+
+     */
+
+
 }
