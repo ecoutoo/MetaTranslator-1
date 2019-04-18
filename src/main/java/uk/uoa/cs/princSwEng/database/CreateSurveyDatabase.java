e0a95d34288fef59057bbdfcd59abc32eb0b7787
nuccirossi <nuccirossi@gmail.com>
2019-04-18 19:03:03 +0100

    Updated CreateSurveyDatabase

--- src/main/java/uk/uoa/cs/princSwEng/database/CreateSurveyDatabase.java
+++ src/main/java/uk/uoa/cs/princSwEng/database/CreateSurveyDatabase.java
@@ -8,8 +8,7 @@ import java.util.List;
 import java.util.ArrayList;
 import uk.uoa.cs.princSwEng.resource.Survey;
 
-public class CreateSurveyDatabase
-{
+public class CreateSurveyDatabase {
 	//private static final String STATEMENT = "INSERT INTO USERSURVEYRECORD (user_id, survey_id) VALUES (?,?) RETURNING *";
 	private static final String STATEMENT5 = "INSERT INTO SURVEYS (user_id, corpora, translator, languages, num, sent1, sent2, sent3, sent4, sent5) VALUES (?,?,?,?,?,?,?,?,?,?) RETURNING *";
 	private static final String STATEMENT10 = "INSERT INTO SURVEYS (user_id, corpora, translator, languages, num, sent1, sent2, sent3, sent4, sent5, sent6, sent7, sent8, sent9, sent10) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) RETURNING *";
@@ -17,8 +16,7 @@ public class CreateSurveyDatabase
 	private final Connection con;
 	private final Survey surv;
 
-	public CreateSurveyDatabase(final Connection con, Survey survey)
-	{
+	public CreateSurveyDatabase(final Connection con, Survey survey) {
 		this.con = con;
 		this.surv = survey;
 	}
@@ -29,71 +27,46 @@ public class CreateSurveyDatabase
  * @throws SQLException
  *             if any error occurs while storing the Category.
  */
-	public int createSurvey() throws SQLException
-	{
+	public int createSurvey() throws SQLException {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		int result = -1;
+		int sentnum = surv.getSurveyNum();
 
-		try 
-		{
-			switch (surv.getSurveyNum())
-			{
+		try {
+			switch (sentnum) {
 				case 5:
 					pstmt = con.prepareStatement(STATEMENT5);
-					pstmt.setInt(1, surv.getSurveyRkey());
-					pstmt.setString(2, surv.getSurveyCorpora());
-					pstmt.setString(3, surv.getSurveyTranslator());
-					pstmt.setString(4, surv.getSurveyLanguages());
-					pstmt.setInt(5, surv.getSurveyNum());
-					for (int i=0; i<5; i++)
-						pstmt.setInt(i+6, surv.getSurveyId()[i]);
 					break;
 				case 10:
 					pstmt = con.prepareStatement(STATEMENT10);
-					pstmt.setInt(1, surv.getSurveyRkey());
-					pstmt.setString(2, surv.getSurveyCorpora());
-					pstmt.setString(3, surv.getSurveyTranslator());
-					pstmt.setString(4, surv.getSurveyLanguages());
-					pstmt.setInt(5, surv.getSurveyNum());
-					for (int i=0; i<10; i++)
-						pstmt.setInt(i+6, surv.getSurveyId()[i]);
 					break;
 				case 15:
 					pstmt = con.prepareStatement(STATEMENT15);
-					pstmt.setInt(1, surv.getSurveyRkey());
-					pstmt.setString(2, surv.getSurveyCorpora());
-					pstmt.setString(3, surv.getSurveyTranslator());
-					pstmt.setString(4, surv.getSurveyLanguages());
-					pstmt.setInt(5, surv.getSurveyNum());
-					for (int i=0; i<15; i++)
-						pstmt.setInt(i+6, surv.getSurveyId()[i]);	
 					break;
-					default: 
+				default: 
 					System.err.println("Error, switch case default value in CreateSurveyDatabase");
 			}
+			pstmt.setInt(1, surv.getSurveyRkey());
+			pstmt.setString(2, surv.getSurveyCorpora());
+			pstmt.setString(3, surv.getSurveyTranslator());
+			pstmt.setString(4, surv.getSurveyLanguages());
+			pstmt.setInt(5, surv.getSurveyNum());
+			for (int i=0; i<sentnum; i++)
+				pstmt.setInt(i+6, surv.getSurveyId()[i]);
 			System.err.println("Before executing executeQuery on Surveys in CreateSurveyDatabase");
+			
 			rs = pstmt.executeQuery();
-
-			if (rs.next())
-			{
+			if (rs.next()) {
 				result = rs.getInt("id");
 			}
-			//pstmt = con.prepareStatement(STATEMENT);
-			//pstmt.setInt(1,surv.getSurveyRkey());
-			//pstmt.setInt(2,result);
-			//System.err.println("Before executing executeQuery on UserSurveyRecord in CreateSurveyDatabase");
-			//rs = pstmt.executeQuery();
 		}
-		catch(SQLException ex)
-		{
+		catch(SQLException ex) {
 			System.err.println("SQLException invoked by the try block in CreateSurveyDatabase");
 			System.err.println(ex.getMessage());
 		}
-		finally
-		{
-			if (pstmt != null)
-			{
+		finally {
+			if (pstmt != null) {
 				pstmt.close();
 			}
 			con.close();
